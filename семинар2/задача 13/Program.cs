c14719f578a4fc4774f0c578a8c363ae0361c6d9
kreper <kreper375@gmail.com>
2022-10-21 22:57:51 +0300

    first

---
+++ семинар2/задача 13/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру (справа налево) заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine()!);
+while (num>1000)
+{
+    num/=10;
+}
+int num1 = num % 10;
+Console.WriteLine($"Третья цифра: {num1}");
