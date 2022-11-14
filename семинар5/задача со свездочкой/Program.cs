75d9c43fc0d1683a6a0b2b3e5c6fa968641a4355
kreper <kreper375@gmail.com>
2022-11-14 23:07:15 +0300

    lesson5

--- семинар5/задача со свездочкой/Program.cs
+++ семинар5/задача со свездочкой/Program.cs
@@ -1,2 +1,27 @@
 ﻿//Задача "со звездочкой": Разобраться с алгоритмом сортировки методом пузырька. Реализовать невозрастающую сторировку.
 Console.Clear();
+ int [] numbers = {1,2,2,3,2};
+int temp;  
+ 
+
+for (int i = 0; i < numbers.Length; i++){
+for(int j = 0; j < numbers.Length-1-i; j++){
+if (numbers[j] < numbers[j + 1]){
+
+temp = numbers[j];
+numbers[j] = numbers[j+1];
+numbers[j+1] = temp;
+}
+}
+}
+
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + ", ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
